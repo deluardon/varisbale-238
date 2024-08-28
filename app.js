9a416ca5f9ea0de33421d262248d6765dea903ad
deluardon <160032360+deluardon@users.noreply.github.com>
2024-08-29 01:24:20 +0600

    done

---
+++ app.js
@@ -0,0 +1,19 @@
+var x = 2;
+// keyword   name      eculation    value          
+    var      y         =            3              ;
+
+x = 5;
+// keyword
+     var       z = x + y;
+console.log(z);
+
+
+
+
+
+var name ="John";
+
+var addreas = "Dhaka";
+
+console.log(name,addreas);
+
---
+++ number.js
@@ -0,0 +1,23 @@
+// var x = parseInt("3");
+var x = parseFloat("5.5");
+var y = 3;
+// console.log(x + y);
+// console.log(typeof x);
+// console.log( x);
+
+var price =5000;
+
+// price=price+1000;
+
+// price+=1000
+
+price-=1000
+
+// console.log(price);
+
+
+
+var sum = 0.1;
+var num = 0.2;
+var z = sum.toFixed(1) + num.toFixed(1);
+console.log(z);
